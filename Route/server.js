88a233f1218b9ae596d2d7760d2685316a05e21e
Eijkeyal Pakhrin <209868212+Eijkeyal@users.noreply.github.com>
2026-07-17 17:30:30 +0545

    Add files via upload

---
+++ Route/server.js
@@ -0,0 +1,60 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // GET route
+  if (req.url === "/users" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Users API",
+      }),
+    );
+  }
+
+  // POST route
+  else if (req.url === "/users" && req.method === "POST") {
+    res.writeHead(200, { "Content-Type": "json" });
+    res.end(
+      JSON.stringify({
+        message: "User created",
+      }),
+    );
+  }
+  //for PUT route
+  else if (req.url === "/users" && req.method === "PUT") {
+    res.writeHead(200, { "content-type": "json" });
+    res.end(
+      JSON.stringify({
+        message: "Data Updated",
+      }),
+    );
+  }
+  //for Delete Route
+  else if (req.url === "/users" && req.method === "DELETE") {
+    res.writeHead(200, { "content-type": "json" });
+    res.end(
+      JSON.stringify({
+        message: "Users Deleted",
+      }),
+    );
+  } else {
+    res.writeHead(404);
+    res.end("Page not Available");
+  }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`This server is running on http://localhost:${PORT}`);
+});
+/*
+const http = require('http');
+const server1 = createServer((req,res)=>{
+
+});
+const PORT1 = 3000;
+server1.listen.apply(PORT,()=>{
+
+});
+*/
